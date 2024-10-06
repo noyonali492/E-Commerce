f153301f3f64f567423481b6463b8c3857084ab9
noyonali492 <noyonahmed492@gmail.com>
2024-10-06 23:04:22 +0600

    Home Page Add

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
