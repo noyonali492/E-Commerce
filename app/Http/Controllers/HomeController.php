a69bcf38b484c8aa19686b4f46ace14703aec19f
noyonali492 <noyonahmed492@gmail.com>
2024-10-29 00:12:32 +0600

    slider home page view dynamic

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Coupon;
+use App\Models\Order;
+use App\Models\Slide;
+use App\Models\OrderItem;
+use App\Models\Transaction;
+use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\File;
+use Intervention\Image\Laravel\Facades\Image;
 
 class HomeController extends Controller
 {
@@ -10,6 +22,7 @@ class HomeController extends Controller
  
     public function index()
     {
-        return view('index');
+        $slides = Slide::where('status',1)->get()->take(3);
+        return view('index',compact('slides'));
     }
 }
