663b3e4b9c523de7319c4653baaaae8e484c6bc8
noyonali492 <noyonahmed492@gmail.com>
2024-10-30 22:29:43 +0600

    4 /c/xampp/htdocs/E-Commerce (main) $ git commit -m

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,7 @@ class HomeController extends Controller
     public function index()
     {
         $slides = Slide::where('status',1)->get()->take(3);
-        return view('index',compact('slides'));
+        $categories = Category::orderBy('name')->get();
+        return view('index',compact('slides','categories'));
     }
 }
